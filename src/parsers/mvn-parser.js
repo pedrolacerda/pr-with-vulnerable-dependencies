a783d4641f5f83f60f232b2bf63f2116812fdba1
Pedro Lacerda <pedrolacerda@github.com>
2020-06-23 21:24:43 -0300

    Testing XML parser

--- src/parsers/mvn-parser.js
+++ src/parsers/mvn-parser.js
@@ -4,50 +4,32 @@ const DOMParser = require('xmldom').DOMParser;
 const apiCalls = require('../api/api-calls.js')
 
 module.exports = {
-    getVulnerabilities: async function (dependencyFile,ecosystem) {
+    getVulnerabilities: async function (dependencyFile, ecosystem) {
+        if(dependencyFile.search('.version') == -1){
 
-        let parser = new DOMParser()
-        let xmlDoc = parser.parseFromString(dependencyFile)
-        
-        // // These are the two tags that add packages to the repo
-        let groupIds = xmlDoc.getElementsByTagName('groupId')
-        let artifactIds = xmlDoc.getElementsByTagName('artifactId')
-        let artifactVersions = xmlDoc.getElementsByTagName('version')
+        } else {
 
-        for(i = 0; i < groupIds["$$length"]; i++) {
-
-            let package = `${groupIds[i]['childNodes']}:${artifactIds[i]['childNodes']}`
-            let version = getVersionValue(artifactVersions[i]['childNodes'], xmlDoc)
-            let hasVulnerabilities = false
-            let minimumVersion = ""
-            
-            // Loops over the list of vulnerabilities of a package
-            apiCalls.getVulnerability(package, ecosystem).then( async function(values) {
-                if(typeof values !== "undefined"){
-                    minimumVersion = "0.0"
-
-                    let vulerabilities = values.securityVulnerabilities.nodes
-
-                    vulerabilities.forEach( vulnerability => {
-                        if(vulnerability.firstPatchedVersion != null && typeof vulnerability.firstPatchedVersion !== 'undefined'){
-                            
-                            // If the version of the package used is lower than the first patched version
-                            // AND the first patched version of the package is bigger than minimun version registered so far
-                            if((semver.compare(semver.valid(semver.coerce(version.toString())), semver.valid(semver.coerce(vulnerability.firstPatchedVersion.identifier.toString()))) == -1)
-                            && (semver.compare(semver.valid(semver.coerce(vulnerability.firstPatchedVersion.identifier.toString())), semver.valid(semver.coerce(minimumVersion.toString()))) == 1)){
-                                minimumVersion = vulnerability.firstPatchedVersion.identifier
-                                hasVulnerabilities = true
-                            }
-                        }
-                    })
-                    if(hasVulnerabilities) core.setFailed(`There's a vulnerability in the package ${package}, please update to version ${minimumVersion}`)
-
-                }
-            }).catch( error => {
-                core.setFailed(error.message)
-                console.log(error)
-            })
         }
+        //Breaks diff into lines for easier manipulation
+        var lines = dependencyFile.split('\n');
+        var listOfChangedDependencies = [];
+        let parser = new DOMParser()
+
+        //Checks if there's a diff for pom.xml files
+        for(line in lines) {
+            if(lines[line].startsWith('+++ b/') && lines[line].endsWith('pom.xml')) {
+                console.log('Line['+line+'] begins the dependencies changes')
+                //moves to the line with diff
+                line = parseInt(line)
+                line += 2
+                // let getOpenTag = line.indexOf('<dependency>')
+                // let getCloseTag = line.indexOf('</dependency>')
+                let xmlDoc = parser.parseFromString(lines[line])
+                console.log(xmlDoc)
+
+            }
+        }
+
     }
 }
 
